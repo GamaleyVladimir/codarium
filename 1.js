c7cf82ebb6678203fb64683c7a140bd20c91c66c
GamaleyVladimir <61056248+GamaleyVladimir@users.noreply.github.com>
2020-03-13 16:51:11 +0200

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,11 @@
+<script type="text/javascript"></script>
+
+function getYear(){
+    var selectEl=document.getElementById("yearR");
+    var yearS = selectEl.value; } 
+function getMounth(){
+    var selectEl=document.getElementById("mounthR");
+    var mounthS = selectEl.value; }
+function getDay(){
+    var selectEl=document.getElementById("dayR");
+    var dayS = selectEl.value; } 
---
+++ js/1.js
@@ -0,0 +1,9 @@
+function getYear(){
+    var selectEl=document.getElementById("yearR");
+    var yearS = selectEl.value; } 
+function getMounth(){
+    var selectEl=document.getElementById("mounthR");
+    var mounthS = selectEl.value; }
+function getDay(){
+    var selectEl=document.getElementById("dayR");
+    var dayS = selectEl.value; } 
